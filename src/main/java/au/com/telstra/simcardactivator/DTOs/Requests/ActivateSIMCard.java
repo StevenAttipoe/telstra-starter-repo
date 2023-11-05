dad23bb6871605b1505c04fa0fe395688a938942
Steven Attipoe <stevenattipoe7@gmail.com>
2023-11-05 22:12:57 +0000

    implemented sim activation feature

---
+++ src/main/java/au/com/telstra/simcardactivator/DTOs/Requests/ActivateSIMCard.java
@@ -0,0 +1,27 @@
+package au.com.telstra.simcardactivator.DTOs.Requests;
+
+public class ActivateSIMCard {
+    private String iccid;
+    private String customerEmail;
+
+    public ActivateSIMCard(String iccid, String customerEmail) {
+        this.iccid = iccid;
+        this.customerEmail = customerEmail;
+    }
+
+    public String getIccid() {
+        return iccid;
+    }
+
+    public void setIccid(String iccid) {
+        this.iccid = iccid;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+}
